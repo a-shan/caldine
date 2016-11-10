2a2d72853b106ca0da162639f27aba2c8be6dc4b
Kai Li Yen <kailiyen@berkeley.edu>
2016-11-10 00:36:08 +0000

    set up devise for users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def show
+    @person = current_user
+  end
+  
+  def save_name
+    current_user.name = params[:name]
+    current_user.save!
+    redirect_to root
+  end
+end
