2d4e58c3977710a8491e04987dc212e51e7e81e1
Alex Shan <alexshan@berkeley.edu>
2016-11-28 08:02:48 +0000

    merge changes to user profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,19 @@ class UsersController < ApplicationController
   def index
     @user = current_user
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
+  
+  def user_params
+    params.require(:user).permit!
+  end
+  
+  def destroy
+  	@user = User.find(params[:id])
+  	@user.destroy
+  	redirect_to meet_ups_path, notice => "Your account have been deleted"
+  end
+
 end
