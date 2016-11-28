8f186881c6601fa6be26c71d7a42783c80f19ee0
shillen102 <atp002@berkeley.edu>
2016-11-27 22:56:05 -0800

    second

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def user_params
+    params.require(:user).permit!
+  end
   def destroy
   	@user = User.find(params[:id])
   	@user.destroy
   	redirect_to meet_ups_path, notice => "Your account have been deleted"
   end
+
 end
