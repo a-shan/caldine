35f3cec2f9ed3612f190227160b1da2faa39c931
anh pham <anh pham>
2016-11-27 21:51:31 -0800

    delete account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,5 +7,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-  
+  def destroy
+  	@user = User.find(params[:id])
+  	@user.destroy
+  	redirect_to meet_ups_path, notice => "Your account have been deleted"
+  end
 end
