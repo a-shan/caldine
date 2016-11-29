aa98d5708d70a32611f2264650c40ad1ef9f0a4d
Alex Shan <alexshan@berkeley.edu>
2016-11-29 01:51:38 +0000

    uploader cache

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,4 +1,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
+  def cache_dir
+    "#{Rails.root}/tmp/uploads"
+  end
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
